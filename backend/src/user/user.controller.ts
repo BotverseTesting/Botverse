ca74518ada062aeb27e9b5b5e3b1c9c27a5f0bc3
Manuel García Ruiz <redfinger@Mac.home>
2025-02-02 22:10:10 +0100

    feature/user-aunthentication basic CRUD

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(
+    @Body()
+    createUserDto: {
+      email: string;
+      password: string;
+      name: string;
+      role: 'SUPERUSER' | 'ADMIN' | 'USER';
+      profilePicture?: string;
+    },
+  ): Promise<User> {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get('all')
+  async getUsers(): Promise<User[]> {
+    return this.userService.getUsers();
+  }
+  @Get(':id')
+  async getUserById(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    const user = await this.userService.getUserById(id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
+  }
+  @Delete(':id')
+  async deleteUser(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    await this.userService.deleteUser(id);
+  }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(data: {
+    email: string;
+    password: string;
+    name: string;
+    role: 'ADMIN' | 'SUPERUSER' | 'USER';
+    profilePicture?: string;
+  }): Promise<User> {
+    return this.prisma.user.create({
+      data: {
+        email: data.email,
+        password: data.password,
+        name: data.name,
+        role: data.role,
+        profilePicture: data.profilePicture,
+      },
+    });
+  }
+
+  async getUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  async getUserById(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+  async deleteUser(id: number): Promise<void> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!user) {
+      throw new Error(`User with id ${id} not found`);
+    }
+    await this.prisma.user.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
+}
