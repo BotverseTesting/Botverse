a3b447a095cb66ef267994d8dae24e199d171c35
Manuel García Ruiz <138726244+Manuelgruiz@users.noreply.github.com>
2025-02-16 18:54:02 +0100

    Revert "feature/githubBots fetch Api Github"

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BotsModule } from './bots/bots.module';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BotsModule, BotsModule, PrismaModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
