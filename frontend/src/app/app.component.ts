cfec35f4e832b209d05e17f3f7b10d1623c5b540
Manuel García Ruiz <redfinger@MacBook-Pro-de-Manuel.local>
2025-03-16 20:12:22 +0100

    epic/homePage Home page and login/logout/register configuration

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  
 })
 export class AppComponent {
-  title = 'frontend';
+  
+  
 }
